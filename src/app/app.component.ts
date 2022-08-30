42babad3beccb26c65bc3cab91353c6446c6f706
Dem0P4n <ch.avendano@duocuc.cl>
2022-08-30 17:55:43 -0400

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,10 @@ export class AppComponent {
     detalle: '',
     imagen: '',
   }
-  public imagen: string = 'https://ecrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcWfAWnFH7_EMaELdteQe9CkK_znlj8I-iHg&usqp=CAU';
-  public imagen2: string = 'https://enrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9051vGf0VDRdPs4iMnqXmxRgOlMDKwAVGjA&usqp=CAU';
-  public imagen3: string = 'https://encypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-gbJ8vil8vZTga21IaoE7xLYByC_ULdQ_rw&usqp=CAU';
+  public elementosGuardados: Array<HistoriaImagen> = [];
+  public imagen: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcWfAWnFH7_EMaELdteQe9CkK_znlj8I-iHg&usqp=CAU';
+  public imagen2: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9051vGf0VDRdPs4iMnqXmxRgOlMDKwAVGjA&usqp=CAU';
+  public imagen3: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-gbJ8vil8vZTga21IaoE7xLYByC_ULdQ_rw&usqp=CAU';
 
   public cambiarNombre(evento: Event): void{
     const elemento = evento.target as HTMLInputElement;
@@ -30,5 +31,18 @@ export class AppComponent {
     const elemento = evento.target as HTMLInputElement;
     this.nuevoHistorial.imagen = elemento.value;
   }
+
+  public guardarHistorial(): void{
+    const copia: HistoriaImagen = {
+      ...this.nuevoHistorial
+    }
+    copia.id = this.elementosGuardados.length + 1;
+    this.elementosGuardados.push(copia);
+    this.nuevoHistorial.nombre = '';
+    this.nuevoHistorial.detalle = '';
+    this.nuevoHistorial.imagen = '';
+
+  }
+
 
 }
